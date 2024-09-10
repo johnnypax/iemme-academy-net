2aaa01412ae08dddfa2d7c255fc68a50952335b2
Giovanni Pace <g.pace@braintechinnovations.it>
2024-09-10 09:28:57 +0200

    Primo commit

---
+++ Lez01_01_introduzione/lez00_introduzione/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace lez00_introduzione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singola linea di commento
+            /*
+             * Commento 
+             * multi linea
+             */
+
+
+#if DEBUG
+            Console.WriteLine("Sono in modalità Debug");
+#elif RELEASE
+            Console.WriteLine("Sono in modalità Release");
+#endif
+
+        }
+    }
+}
