9dc75d3f7af1ad92b8bfa392deab3242e55d731d
Giovanni Pace <g.pace@braintechinnovations.it>
2024-09-17 17:53:01 +0200

    Aggiunta MTM

---
+++ Lez_SQL/lez07_mtm_scuola.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE a_lez07_mtm_universita;
+USE a_lez07_mtm_universita;
+
+CREATE TABLE Studente(
+	studenteID INTEGER PRIMARY KEY IDENTITY(1,1),
+	nominativo VARCHAR(100) NOT NULL,	
+	matricola VARCHAR(10) NOT NULL UNIQUE
+);
+
+CREATE TABLE Esame(
+	esameID INTEGER PRIMARY KEY IDENTITY(1,1),
+	titolo VARCHAR(250) NOT NULL,
+	data_esame DATETIME NOT NULL,
+	crediti INTEGER
+);
+
+CREATE TABLE Studente_Esame(
+	studenteRIF INTEGER NOT NULL,
+	esameRIF INTEGER NOT NULL,
+	FOREIGN KEY(studenteRIF) REFERENCES Studente(studenteID) ON DELETE CASCADE,
+	FOREIGN KEY(esameRIF) REFERENCES Esame(esameID) ON DELETE CASCADE,
+	PRIMARY KEY(studenteRIF, esameRIF)
+);
