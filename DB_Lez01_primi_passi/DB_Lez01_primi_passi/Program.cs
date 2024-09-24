f34d39bfe4df28d1306e3a17bfb07af91ae0c0ca
Giovanni Pace <g.pace@braintechinnovations.it>
2024-09-24 11:41:22 +0200

    Aggiunta gestione DB

---
+++ DB_Lez01_primi_passi/DB_Lez01_primi_passi/Program.cs
@@ -0,0 +1,61 @@
+﻿using DB_Lez01_primi_passi.classes;
+using System.Data.SqlClient;
+
+namespace DB_Lez01_primi_passi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string credenziali = "Server=BOOK-N57JVKH6HJ\\SQLEXPRESS;Database=db_lez01_azienda;User Id=academy;Password=academy!;MultipleActiveResultSets=true;Encrypt=false;TrustServerCertificate=false";
+
+            List<Dipendente> elenco = new List<Dipendente>();
+
+
+            using (SqlConnection connessione = new SqlConnection(credenziali))
+            {
+                string query = "SELECT dipententeID, nome, cognome, dipartimento, identificativo, salario FROM Dipendente;";
+
+                SqlCommand cmd = new SqlCommand(query, connessione);
+
+                try
+                {
+                    connessione.Open();
+
+                    SqlDataReader dati = cmd.ExecuteReader();
+                    while (dati.Read())
+                    {
+                        //Console.WriteLine($"{dati[0]} {dati[1]}");
+
+                        Dipendente dip = new Dipendente()
+                        {
+                            Id = dati.GetInt32(0),
+                            Nome = dati.GetString(1),
+                            Cogn = dati.GetString(2),
+                            Dipa = dati.GetString(3),
+                            Iden = dati.GetString(4),
+                            Sala = dati.GetInt32(5)
+                        };
+
+                        elenco.Add(dip);
+                    }
+
+                    connessione.Close();
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+            }
+
+
+            foreach (Dipendente dip in elenco)
+            {
+                Console.WriteLine(dip);
+            }
+
+            Console.WriteLine("FINE DEL PROGRAMMA");
+
+        }
+    }
+}
---
+++ DB_Lez01_primi_passi/DB_Lez01_primi_passi/classes/Dipendente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_Lez01_primi_passi.classes
+{
+    internal class Dipendente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = null!;
+        public string Cogn { get; set; } = null!;
+        public string? Dipa { get; set; }
+        public string Iden { get; set; } = null!;
+        public int Sala { get; set; }
+
+        public override string ToString()
+        {
+            return $"[DIPENDENTE] {Id} {Nome} {Cogn} {Dipa} {Iden} {Sala}";
+        }
+    }
+}
