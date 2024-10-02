ff954267557def38bd3c11e904df4e8f0144547c
Giovanni Pace <g.pace@braintechinnovations.it>
2024-10-02 15:32:12 +0200

    Aggiunta gestione funzioni

---
+++ Lez_HTML/lez16_variabili/script.js
@@ -0,0 +1,18 @@
+console.log("Ciao Giovanni");
+console.log("Ciao Giovanni");
+console.log("Ciao Giovanni");
+
+debugger;
+console.log("Ciao Giovanni");
+console.log("Ciao Giovanni");
+console.log("Ciao Giovanni");
+console.log("Ciao Giovanni");
+
+var a = 5;
+var b = 6;
+
+if(a > b){
+    console.log("Maggiore");
+} else{
+    console.log("Minore");
+}
---
+++ Lez_HTML/lez20_scoping/scriptUno.js
@@ -0,0 +1,8 @@
+// function somma(varUno, varDue){
+//     return varUno + varDue;
+// }
+
+// let a = 5;
+// let b = 6;
+
+// console.log(somma(a,b));
