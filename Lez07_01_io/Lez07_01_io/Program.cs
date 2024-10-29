1423148ed870dd7d8fb82f94192088e21951b723
Giovanni Pace <g.pace@braintechinnovations.it>
2024-10-29 09:34:03 +0100

    Aggiunta gestione file

---
+++ Lez07_01_io/Lez07_01_io/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Lez07_01_io
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Scrittura su file
+            //string path = "C:\\Users\\ACADEMY\\Desktop\\provaprova.txt";
+            ////string? path = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+            //Console.WriteLine(path);
+
+            //string contenuto = "Ciao sono Giovanni Pace";
+
+            //try
+            //{
+            //    if(path is not null)
+            //        using(StreamWriter sw = new StreamWriter(path))
+            //        {
+            //            sw.WriteLine(contenuto);
+
+            //            sw.WriteLine("CIAOCIAO");
+
+            //            sw.Close();
+            //        }
+
+            //    Console.WriteLine("STAPPOOOOOOO");
+
+            //} catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);   
+            #endregion
+
+
+            string path = "C:\\Users\\ACADEMY\\Desktop\\provaprova.txt";
+            try
+            {
+                using(StreamReader sr = new StreamReader(path))
+                {
+                    string? line;
+
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
